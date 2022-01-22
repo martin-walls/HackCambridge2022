d4032f94f3a341df65b2d6f33d0bfe3373b52d31
ql309 <ql309@cam.ac.uk>
2022-01-22 18:17:36 +0000

    Use Drone and Person objects when updating.

--- backend.py
+++ backend.py
@@ -1,5 +1,3 @@
-
-
 class UpdateAttrs:
     peopleList = []
     droneList = []
@@ -10,35 +8,35 @@ class UpdateAttrs:
         self.droneList = listOfDroneObjects
 
 
-
-
 class Drone:
     x = 0
     y = 0
+
     def __init__(self):
         """ Creates drone object """
-        self.x = 0
-        self.y = 0
+        self.x = 70
+        self.y = 90
 
     def returnCoords(self):
         return (self.x, self.y)
+
 
 class Person:
     x = 0
     y = 0
+
     def __init__(self):
         """ Creates person object """
-        self.x = 0
-        self.y = 0
+        self.x = 100
+        self.y = 240
 
     def returnCoords(self):
         return (self.x, self.y)
 
 
-
-
 def update():
     retAttr = UpdateAttrs([Person()], [Drone()])
-    return retAttr
+    return (retAttr.peopleList, retAttr.droneList)
+
 
 print(update())
--- simulation.py
+++ simulation.py
@@ -3,16 +3,16 @@ import sys
 
 import backend
 
-SIZE = width, height = 700,500
+SIZE = width, height = 700, 500
 
 # COLORS
-OCEAN = (0,0,255)
-BLACK = (0,0,0)
+OCEAN = (0, 0, 255)
+BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 
 PERSON_FILL = BLACK
 DRONE_FILL = WHITE
-DRONE_SEARCH_RADIUS = (255,255,255,128)
+DRONE_SEARCH_RADIUS = (255, 255, 255, 128)
 
 screen = pygame.display.set_mode(SIZE)
 fps = pygame.time.Clock()
@@ -21,19 +21,32 @@ fps = pygame.time.Clock()
 def draw_person(pos):
     pygame.draw.circle(screen, PERSON_FILL, pos, 5, 0)
 
+
 def draw_drone(pos, search_radius):
     pygame.draw.circle(screen, DRONE_FILL, pos, 3, 0)
     pygame.draw.circle(screen, DRONE_SEARCH_RADIUS, pos, search_radius, 1)
 
-def render(person_pos, drone_pos):
+
+def render(person_list, drone_list):
     screen.fill(OCEAN)
-    draw_person(person_pos)
-    for drone in drone_pos:
-        draw_drone(drone, 10)
+
+    for person in person_list:
+        draw_person(person.returnCoords())
+
+    for drone in drone_list:
+        draw_drone(drone.returnCoords(), 10)
+
     pygame.display.update()
     fps.tick(60)
 
 
+# def render(person_pos, drone_pos):
+#     screen.fill(OCEAN)
+#     draw_person(person_pos)
+#     for drone in drone_pos:
+#         draw_drone(drone, 10)
+#     pygame.display.update()
+#     fps.tick(60)
 
 
 if __name__ == "__main__":
@@ -45,4 +58,4 @@ if __name__ == "__main__":
                 sys.exit()
 
         attrs = backend.update()
-        render(attrs.person_pos, attrs.drone_positions)
+        render(attrs[0], attrs[1])
