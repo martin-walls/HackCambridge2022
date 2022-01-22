c30c2ff55ccccbabe6403bbca953fb5ed206485d
Martin Walls <44908692+martin-walls@users.noreply.github.com>
2022-01-22 17:56:42 +0000

    .

--- simulation.py
+++ simulation.py
@@ -10,29 +10,38 @@ OCEAN = (0,0,255)
 BLACK = (0,0,0)
 WHITE = (255, 255, 255)
 
+DRONE_FILL = WHITE
+DRONE_SEARCH_RADIUS = WHITE
+
 screen = pygame.display.set_mode(SIZE)
 fps = pygame.time.Clock()
 
 
 def draw_person(pos):
-  pygame.draw.circle(screen, BLACK, pos, 5, 0)
+    pygame.draw.circle(screen, BLACK, pos, 5, 0)
 
-def draw_drone(pos):
-  pygame.draw.circle(screen, WHITE, pos, 3, 0)
+def draw_drone(pos, search_radius):
+    pygame.draw.circle(screen, WHITE, pos, 3, 0)
+    pygame.draw.circle(screen, WHITE, pos, search_radius, 1)
 
 def render(person_pos, drone_pos):
-  screen.fill(OCEAN)
-  draw_person(person_pos)
-  for drone in drone_pos:
-    draw_drone(drone)
-  pygame.display.update()
-  fps.tick(60)
+    screen.fill(OCEAN)
+    draw_person(person_pos)
+    for drone in drone_pos:
+        draw_drone(drone, 10)
+    pygame.display.update()
+    fps.tick(60)
 
 
 
 
 if __name__ == "__main__":
 
-  while True:
-    attrs = backend.update()
-    render(attrs.person_pos, attrs.drone_positions)
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+
+        attrs = backend.update()
+        render(attrs.person_pos, attrs.drone_positions)
