58ee83714c6e8c260cda8aa68870f33cb1281ef7
Martin Walls <44908692+martin-walls@users.noreply.github.com>
2022-01-22 17:58:26 +0000

    .

--- simulation.py
+++ simulation.py
@@ -10,19 +10,20 @@ OCEAN = (0,0,255)
 BLACK = (0,0,0)
 WHITE = (255, 255, 255)
 
+PERSON_FILL = BLACK
 DRONE_FILL = WHITE
-DRONE_SEARCH_RADIUS = WHITE
+DRONE_SEARCH_RADIUS = (255,255,255,128)
 
 screen = pygame.display.set_mode(SIZE)
 fps = pygame.time.Clock()
 
 
 def draw_person(pos):
-    pygame.draw.circle(screen, BLACK, pos, 5, 0)
+    pygame.draw.circle(screen, PERSON_FILL, pos, 5, 0)
 
 def draw_drone(pos, search_radius):
-    pygame.draw.circle(screen, WHITE, pos, 3, 0)
-    pygame.draw.circle(screen, WHITE, pos, search_radius, 1)
+    pygame.draw.circle(screen, DRONE_FILL, pos, 3, 0)
+    pygame.draw.circle(screen, DRONE_SEARCH_RADIUS, pos, search_radius, 1)
 
 def render(person_pos, drone_pos):
     screen.fill(OCEAN)
