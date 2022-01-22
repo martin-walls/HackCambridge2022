54d2b3e76fa32c76a9e6142769fcb3916f805fc4
QianyiLiu309 <liuqianyi0023@gmail.com>
2022-01-22 21:40:07 +0000

    Implement ZShapeAlgorithm()

--- algorithms.py
+++ algorithms.py
@@ -7,25 +7,50 @@ class SearchAlgorithm:
     def returnNextWorldStateInstance(self):
         return self.worldState
 
-class BasicSearchAlgorithm(SearchAlgorithm):
-    dfaState = ""
-    startingWidthLocations = []
-    def __init__(self, WSI):
-        " Creates a basic \"snake like\" algorithm "
-        self.worldState = WSI
-        self.dfaState = "setup"
-        for i in range(len(WSI.listOfDroneObjects)):
-            startingWidthLocations.append((WSI.width/len(WSI.listOfDroneObjects))*i)
+# class BasicSearchAlgorithm(SearchAlgorithm):
+#     dfaState = ""
+#     startingWidthLocations = []
+#     def __init__(self, WSI):
+#         " Creates a basic \"snake like\" algorithm "
+#         self.worldState = WSI
+#         self.dfaState = "setup"
+#         for i in range(len(WSI.listOfDroneObjects)):
+#             self.startingWidthLocations.append((WSI.width/len(WSI.listOfDroneObjects))*i)
+#
+#
+#     def returnNextWorldStateInstance(self):
+#         if (dfaState == "setup"):
+#             # establish starting locations for the drones and move them there
+#             movespeed = listOfDroneObjects[0].moveSpeed
+#             for i in range(len(worldState.listOfDroneObjects)):
+#                 x_loc = listOfDroneObjects[i].x
+#                 if (x_loc != startingWidthLocations[i]):
+#                     # do movemenet of drones towards starting location
+#                     WSI.listOfDroneObjects[i].x += min(startingWidthLocations - x_loc, moveSpeed)
+#
+#         return worldState
 
+
+class ZShapeAlgorithm(SearchAlgorithm):
+    def __init__(self, WSI):
+        self.worldState = WSI
+
+        self.dx = self.worldState.width / self.worldState.numDrones
+        self.dy = WSI.droneList[0].searchRadius
+
+        self.moveToRight = 1
+
+        self.worldState.numDrones = 1
+        for i in range(self.worldState.numDrones):
+            self.worldState.droneList[i].setCoords(int(self.dx * (i + 0.5)), 0)
 
     def returnNextWorldStateInstance(self):
-        if (dfaState == "setup"):
-            # establish starting locations for the drones and move them there
-            movespeed = listOfDroneObjects[0].moveSpeed
-            for i in range(len(worldState.listOfDroneObjects)):
-                x_loc = listOfDroneObjects[i].x
-                if (x_loc != startingWidthLocations[i]):
-                    # do movemenet of drones towards starting location
-                    WSI.listOfDroneObjects[i].x += min(startingWidthLocations - x_loc, moveSpeed)
+        for drone in self.worldState.droneList:
+            drone.moveInDirection(self.dx * self.moveToRight, self.dy)
 
-        return worldState
+        if self.worldState.droneList[0].x >= self.dx or self.worldState.droneList[0].x <= 0:
+            self.moveToRight *= -1
+
+        return self.worldState
+
+
