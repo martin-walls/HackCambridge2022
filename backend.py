c49ad2a5eec3b950e1280ec199a3845c7fbc9bda
Fabien <fb531@cam.ac.uk>
2022-01-22 17:59:23 +0000

    .

--- backend.py
+++ backend.py
@@ -1,8 +1,44 @@
 
 
 class UpdateAttrs:
-  person_pos = []
-  drone_positions = []
+    peopleList = []
+    droneList = []
 
-  def update(self):
-    print("update")
+    def __init__(self, listOfPersonObjects, listOfDroneObjects):
+        """ Create a new point at the origin """
+        self.peopleList = listOfPersonObjects
+        self.droneList = listOfDroneObjects
+
+
+
+
+class Drone:
+    x = 0
+    y = 0
+    def __init__(self):
+        """ Creates drone object """
+        self.x = 0
+        self.y = 0
+
+    def returnCoords(self):
+        return (self.x, self.y)
+
+class Person:
+    x = 0
+    y = 0
+    def __init__(self):
+        """ Creates person object """
+        self.x = 0
+        self.y = 0
+
+    def returnCoords(self):
+        return (self.x, self.y)
+
+
+
+
+def update():
+    retAttr = UpdateAttrs([Person()], [Drone()])
+    return (retAttr.peopleList, retAttr.droneList)
+
+print(update())
