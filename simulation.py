4d2989fa39ca5400b42f05613873c40a1df0bac7
Martin Walls <44908692+martin-walls@users.noreply.github.com>
2022-01-22 18:53:19 +0000

    .

--- simulation.py
+++ simulation.py
@@ -12,10 +12,13 @@ WHITE = (255, 255, 255)
 
 PERSON_FILL = BLACK
 DRONE_FILL = WHITE
-DRONE_SEARCH_RADIUS = (255, 255, 255, 128)
+DRONE_SEARCH_RADIUS_COLOR = (156,156,255)
+DRONE_DOT_RADIUS = 5
 
 screen = pygame.display.set_mode(SIZE)
 fps = pygame.time.Clock()
+
+surface = pygame.Surface(SIZE, pygame.SRCALPHA)
 
 person_img = pygame.image.load("sadface.png").convert_alpha()
 person_img = pygame.transform.scale(person_img, (68,62))
@@ -23,22 +26,24 @@ person_img = pygame.transform.scale(person_img, (68,62))
 
 def draw_person(person):
     # pygame.draw.circle(screen, PERSON_FILL, pos, 5, 0)
-    screen.blit(person_img, person.returnCoords())
+    surface.blit(person_img, person.returnCoords())
 
 
 def draw_drone(drone):
-    pygame.draw.circle(screen, DRONE_FILL, drone.returnCoords(), 3, 0)
-    pygame.draw.circle(screen, DRONE_SEARCH_RADIUS, drone.returnCoords(), drone.getSearchRadius(), 1)
+    pygame.draw.circle(surface, DRONE_FILL, drone.returnCoords(), DRONE_DOT_RADIUS, 0)
+    pygame.draw.circle(surface, DRONE_SEARCH_RADIUS_COLOR, drone.returnCoords(), drone.getSearchRadius(), 1)
 
 
 def render(person_list, drone_list):
-    screen.fill(OCEAN)
+    surface.fill(OCEAN)
 
     for person in person_list:
         draw_person(person)
 
     for drone in drone_list:
         draw_drone(drone)
+
+    screen.blit(surface, (0,0))
 
     pygame.display.update()
     fps.tick(60)
