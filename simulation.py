d15c563325e11acb3327a70bd63f7fb00a2c1166
Martin Walls <44908692+martin-walls@users.noreply.github.com>
2022-01-22 19:01:53 +0000

    drone img

--- simulation.py
+++ simulation.py
@@ -23,6 +23,9 @@ surface = pygame.Surface(SIZE, pygame.SRCALPHA)
 person_img = pygame.image.load("sadface.png").convert_alpha()
 person_img = pygame.transform.scale(person_img, (68,62))
 
+drone_img = pygame.image.load("drone.png").convert_alpha()
+drone_img = pygame.transform.scale(drone_img, (70,70))
+
 
 def draw_person(person):
     # pygame.draw.circle(screen, PERSON_FILL, pos, 5, 0)
@@ -30,7 +33,8 @@ def draw_person(person):
 
 
 def draw_drone(drone):
-    pygame.draw.circle(surface, DRONE_FILL, drone.returnCoords(), DRONE_DOT_RADIUS, 0)
+    # pygame.draw.circle(surface, DRONE_FILL, drone.returnCoords(), DRONE_DOT_RADIUS, 0)
+    surface.blit(drone_img, (drone.returnCoords()[0] - 35, drone.returnCoords()[1] - 35))
     pygame.draw.circle(surface, DRONE_SEARCH_RADIUS_COLOR, drone.returnCoords(), drone.getSearchRadius(), 1)
 
 
