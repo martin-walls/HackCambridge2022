8d0f237dc7777702596c9f59eeb7aa6dea6dac81
Martin Walls <44908692+martin-walls@users.noreply.github.com>
2022-01-22 17:23:57 +0000

    add backend class

---
+++ backend.py
@@ -0,0 +1,8 @@
+
+
+class UpdateAttrs:
+  person_pos = []
+  drone_positions = []
+
+  def update(self):
+    print("update")
--- simulation.py
+++ simulation.py
@@ -1,34 +1,50 @@
 import pygame
 import sys
 
-size = width, height = 500,500
-speed = [1,1]
+import backend
 
-OCEAN = (0,0,255)
+SIZE = width, height = 500,500
 
-pygame.init()
-screen = pygame.display.set_mode(size)
-fps = pygame.time.Clock()
-
-person_pos = [50, 50]
-move_counter = 0
-
-def render():
-  screen.fill(OCEAN)
-  pygame.draw.circle(screen, (0,0,0), person_pos, 5, 0)
-  pygame.display.update()
-  fps.tick(60)
+# person_position = []
+# drone_positions = [[x, y], ...]
 
 
-while True:
-  for event in pygame.event.get():
-    if event.type == pygame.QUIT:
-      pygame.quit()
-      sys.exit()
 
-  move_counter += 1
-  if (move_counter == 20):
-    person_pos[0] += 1
-    move_counter = 0
+# speed = [1,1]
 
-  render()
+# OCEAN = (0,0,255)
+
+# pygame.init()
+# screen = pygame.display.set_mode(SIZE)
+# fps = pygame.time.Clock()
+
+# person_pos = [50, 50]
+# move_counter = 0
+
+# def render():
+#   screen.fill(OCEAN)
+#   pygame.draw.circle(screen, (0,0,0), person_pos, 5, 0)
+#   pygame.display.update()
+#   fps.tick(60)
+
+
+
+# while True:
+#   for event in pygame.event.get():
+#     if event.type == pygame.QUIT:
+#       pygame.quit()
+#       sys.exit()
+
+#   person_position, drone_positions = backend.update()
+
+#   move_counter += 1
+
+#   if (move_counter == 20):
+#     person_pos[0] += 1
+#     move_counter = 0
+
+#   render()
+
+if __name__ == "__main__":
+  x = backend.UpdateAttrs()
+  print(x.person_pos)
